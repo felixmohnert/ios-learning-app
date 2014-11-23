3aa176df2effb04a0df509bd02d73c513906c2ad
Felix Mohnert <felix@mohnert.de>
2014-11-23 20:28:31 +0100

    Shuffle cards

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var firstCardImageView: UIImageView!
+  @IBOutlet weak var secondCardImageView: UIImageView!
+  @IBOutlet weak var playRoundButton: UIButton!
+  @IBOutlet weak var backgroundImageView: UIImageView!
+  
+  var cardNamesArray:[String] = ["ace", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king"]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+        
+    self.playRoundButton.setTitle("Play", forState: UIControlState.Normal)
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  @IBAction func playRoundTapped(sender: UIButton) {
+    // Randomize a number for the first imageview
+    var firstRandomNumber: Int = Int(arc4random_uniform(13))
+      
+    // Construct a string with the random number
+    var firstCardString: String = self.cardNamesArray[firstRandomNumber]
+      
+    // Set the first card image view ot the asset corresponding to the randomized number
+    self.firstCardImageView.image = UIImage(named: firstCardString)
+      
+    // Randomize a number for the second imageview
+    var secondRandomNumber: Int = Int(arc4random_uniform(13))
+      
+    // Construct a string with the random number
+    var secondCardString: String = self.cardNamesArray[secondRandomNumber]
+      
+    // Set the second card image view ot the asset corresponding to the randomized number
+    self.secondCardImageView.image = UIImage(named: secondCardString)
+    
+    // Determine the higher card
+    if firstRandomNumber > secondRandomNumber {
+      // TODO: First card is larger
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    else if firstRandomNumber == secondRandomNumber
+    else {
+      // TOTO: Sedonc card is larger
     }
-
-
+  }
 }
 
